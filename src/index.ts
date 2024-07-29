583c9d41a0097e028f58905ec16556c1a8d33b79
kotyabuchi <kotyabuchi@gmail.com>
2024-07-30 00:01:00 +0900

    route忘れ

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono';
 import user from './user';
 import scenario from './scenario';
+import tag from './tag';
 
 const app = new Hono<{ Bindings: { DB: D1Database } }>();
 
-app.route('users', user).route('scenarios', scenario);
+app.route('users', user).route('scenarios', scenario).route('tag', tag);
 
 export default app;
